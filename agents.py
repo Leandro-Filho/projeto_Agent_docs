0e31c3825298d749f1c10b1c587055e46140b99b
bebaran <leandro.filho@sou.inteli.edu.br>
2025-08-08 07:37:18 -0300

    feat: Inserção dos arquivos agents.py e scrapper

---
+++ agents.py
@@ -0,0 +1,81 @@
+#importar a url que iremos pegar será fundamental para que o agente saiba o contexto de sua resposta 
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from IA_documentacao.scrapper import get_all_text_from_url 
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+
+def get_reponse_from_openai(message):
+
+    llm = ChatOpenAI(model_name="gpt-3.5-tubro")
+
+    response = llm.invoke(message)
+
+    return response
+
+
+#esse @tool vai fazer com que cada função que tiver @tool em cima, vira uma ferramenta para os agentes usarem
+@tool 
+#aqui fizemos uma função com um prompt responsável por colocar como parâmentro a url e a pergunta que o usuário irá fazer, além disso, o prompt avisa que irá receber uma documnetação de uma certa url e juntamnete uma pergunta sobre ela
+def documentacion_tool(url: str, question: str) -> str:
+    """This tool will receive as input a URL about a documentation and also a question about that documentation in which the user wants to be answered."""
+
+
+    #como dito, dareemos um contexto para o HumanMessage. Fizemos a importação da função e colocamos a url da documentação como parâmetro.
+    context = get_all_text_from_url(url)
+
+    #será feito os prompts para: a resposta seja levada para algo mais técnica por falar que são documentações de programação, mesmo sendo o mais simples possivel. Além disso, um prompt feito para que seja lido e interpretado tanto a url quanto a pergunta feita, fazendo com que o agente interligue a pergunta com o conteúdo da url
+    messages = [
+        SystemMessage(content="You're a helpful programming assistant that must explain programming library documentation to users as simples as possible"),
+        HumanMessage(content=f"Documentation: {context}\n\n {question}")
+    ]
+
+    #aqui será responsável por mostrar para o gpt-3.5-turbo que tem que responder de acordo com a mensagem acima
+    response = get_reponse_from_openai(messages)
+
+    return response
+@tool
+#Essa função irá formatar o arquivo em python. A importância desse aqui é formatar os caminhos e criar uma padronização para que rode em todos os sistemas operacionais, segurança e reuso do código
+def black_formatter(path: str) -> str:
+    """This tool receives as input a file of system path to a python script file and runs black formatter to format the file's python code"""
+
+    try:
+        os.system("poetry run black {path}")
+        return "Feito!"
+    except:
+        return "Não foi feito!"
+
+#simplesmente aqui será onde ficará as 'ferramentas' para o agente 
+toolkit = [documentacion_tool, black_formatter]
+
+#criamos mais uma llm e colocamos temperatuara igual 0 para que não haja criatividade por parte das respostas, respotas mais concretas
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+prompts = ChatPromptTemplate.from_messages(
+    [
+        ("system", """
+            You are a programming assitant. Use your tools to answer questions.
+            If you do not have a toll to answe the question, say to.
+         
+            Return only the answers.
+         """),
+         MessagesPlaceholder("chat_history", optional=True),
+         ("human", "{input}"),
+         MessagesPlaceholder("agent_scratchpad"),
+    ]
+)
+
+#aqui, só passamos e criamos o agente com tudo que ela precisa para performar 
+agent = create_openai_tools_agent(llm, toolkit, prompts)
+
+agent_executor = AgentExecutor(agent=agent, tools=toolkit, verbose=True)
+
+result = agent_executor.invoke({"input": "Olá!"})
+
+print(result["output"])
---
+++ scrapper.py
@@ -0,0 +1,31 @@
+#importamos as duas bibliotecas que usaremos para fazer a pesquisa em uma url
+
+# Será responsável por facilitar o recebimento de dados e informações vinda da internet
+import requests 
+
+#responsável por pegar os dados da url da melhor forma possível
+from bs4 import BeautifulSoup
+
+#essa função irá apenas acessar a url e extrair o texto por inteiro do site
+def get_all_text_from_url(url):
+    reponse = requests.get(url)
+
+#apenas verifica se a busca na url deu certo
+    if reponse.status_code == 200:
+        soup =BeautifulSoup(reponse.content, "html.parser")
+
+#remove tudo que não é o texto que queremos, ou seja, os scripts e styles do site 
+        for scripts_or_styles in soup("scripts", "styles"):
+            scripts_or_styles.decompose()
+
+#pega apenas o texto da pagina
+        text = soup.get_text()
+
+        lines = (line.strip() for line in text.splitlines())
+        chuncks = (phrase.strip() for line in lines for phrase in line.split(""))
+        text = "\n ".join(chunk for chunk in chuncks if chunk)
+
+        return text
+    
+    else:
+        return("erro ao tentar acessar o site: {reponse.status_code}")
